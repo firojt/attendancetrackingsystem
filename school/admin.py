9b21fad4b462754a986d0a3f674bcda01497ef8e
Firoj Timalsina <firojt0@gmail.com>
2021-05-25 16:28:13 -0600

    attendance in percentage

--- school/admin.py
+++ school/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 
-from .models import Attendance, Campus, Course, Department, Student, Teacher
+from .models import Attendance, Course, Student, Teacher
 
-admin.site.register(Campus)
-admin.site.register(Department)
+
 admin.site.register(Student)
 admin.site.register(Teacher)
 admin.site.register(Course)
--- school/models.py
+++ school/models.py
@@ -2,27 +2,6 @@ from django.db import models
 from django.db.models.deletion import CASCADE
 from django.urls import reverse
 
-# Create your models here.
-
-class Campus(models.Model):
-    """Model representing a campus name"""
-    name = models.CharField(max_length=200, help_text='Enter a campus name')
-
-    def __str__(self):
-        """String for representing the Model object."""
-        return self.name
-
-
-class Department(models.Model):
-    """Model representing a campus name"""
-    name = models.CharField(max_length=200, help_text='Enter a department name')
-
-    campus = models.ManyToManyField(Campus, help_text='Select a Campus for this dep. note many to many mapping between campus and department')
-
-    def __str__(self):
-        """String for representing the Model object."""
-        return self.name
-
 class Student(models.Model):
     """Model representing a campus name"""
     id = models.AutoField(primary_key='True', max_length=50)
@@ -57,6 +36,7 @@ class Course(models.Model):
     name = models.CharField(max_length=50)
     classStartDate = models.DateField(default='2021-05-18')
     classEndDate = models.DateField(default='2021-08-10')
+    totalClassDay = models.IntegerField(default=30)
     credit = models.CharField(max_length=50)
     sectionName = models.CharField(max_length=200, help_text='Enter a section name')
     student = models.ForeignKey(Student, on_delete=CASCADE)
@@ -78,15 +58,3 @@ class Attendance(models.Model):
         sname = Student.objects.get(name=self.student)
         cname = Course.objects.get(name=self.course)
         return '%s : %s' % (sname.name, cname.name)
-
-# class Attendance(models.Model):
-#     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-#     student = models.ForeignKey(Student, on_delete=models.CASCADE)
-#     attendanceclass = models.ForeignKey(AttendanceClass, on_delete=models.CASCADE, default=1)
-#     date = models.DateField(default='2018-10-23')
-#     status = models.BooleanField(default='True')
-
-#     def __str__(self):
-#         sname = Student.objects.get(name=self.student)
-#         cname = Course.objects.get(name=self.course)
-#         return '%s : %s' % (sname.name, cname.shortname)
